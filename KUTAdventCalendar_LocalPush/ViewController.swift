8fc35112d59d81b46bd023b76f12435f1562cc6d
Pine Mizune <pinemz@gmail.com>
2017-12-09 16:48:57 +0900

    feat(Push): add local push sample code

--- KUTAdventCalendar_LocalPush/ViewController.swift
+++ KUTAdventCalendar_LocalPush/ViewController.swift
@@ -7,19 +7,46 @@
 //
 
 import UIKit
+import UserNotifications
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
 
-
+    @IBAction func notifyTapped(_ sender: Any) {
+        let content = UNMutableNotificationContent()
+        content.title = "れんちょんからのお知らせ"
+        content.body = "にゃんぱすー"
+        content.sound = UNNotificationSound.default()
+        
+        if let path = Bundle.main.path(forResource: "Renchon", ofType: "jpg") {
+            content.attachments = [try! UNNotificationAttachment(identifier: "renchon", url: URL(fileURLWithPath: path), options: nil)]
+        }
+        
+        let request = UNNotificationRequest(identifier: "immediately", content: content, trigger: nil)
+        UNUserNotificationCenter.current().add(request, withCompletionHandler: nil)
+    }
+    
+    @IBAction func nofityLaterTapped(_ sender: Any) {
+        let content = UNMutableNotificationContent()
+        content.title = "れんちょんからのお知らせ"
+        content.body = "もう朝なのん"
+        content.sound = UNNotificationSound.default()
+        
+        if let path = Bundle.main.path(forResource: "Renchon", ofType: "jpg") {
+            content.attachments = [try! UNNotificationAttachment(identifier: "renchon", url: URL(fileURLWithPath: path), options: nil)]
+        }
+        
+        let trigger = UNTimeIntervalNotificationTrigger(timeInterval: 3, repeats: false)
+        let request = UNNotificationRequest(identifier: "later", content: content, trigger: trigger)
+        UNUserNotificationCenter.current().add(request, withCompletionHandler: nil)
+    }
+    
 }
 
